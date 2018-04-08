c4b59672c7b417cf1f52bf953abfd3030c70f074
Jia Ye <planet0104@gmail.com>
2018-04-08 18:22:13 +0800

    server

--- plan2/server/src/main.rs
+++ plan2/server/src/main.rs
@@ -2,12 +2,12 @@ extern crate ws;
 #[macro_use]
 extern crate json;
 extern crate tank;
-use ws::{WebSocket, CloseCode, listen, Handler, Message, Request, Response, Result, Sender, Handshake};
+use ws::{WebSocket, CloseCode, Handler, Message, Result, Sender, Handshake};
 use json::JsonValue;
 use std::sync::mpsc::channel;
 use std::sync::mpsc::Sender as GameSender;
 use std::time::Duration;
-use tank::{ TankGame, SpriteEvent, SpriteInfo };
+use tank::TankGame;
 use tank::utils::Timer;
 use std::thread;
 
@@ -21,7 +21,7 @@ const MSG_MOUSE_EVENT: isize = 5;
 // 服务器Web处理程序
 struct Player {
     out: Sender,
-    sender: GameSender<(Sender, isize, Option<isize>, Option<isize>)>
+    sender: GameSender<(Sender, isize, JsonValue)>
 }
 
 impl Player{}
@@ -32,42 +32,32 @@ impl Handler for Player {
         println!("客户端连接:{:?}", shake.remote_addr());
 
         //玩家连线，从游戏拉去精灵数据，发送给客户端
-        self.sender.send((self.out, MSG_CONNECT, None, None));
+        self.sender.send((self.out, MSG_CONNECT, JsonValue::Null));
         Ok(())
     }
 
     fn on_close(&mut self, code: CloseCode, reason: &str){
         //玩家下线
-        self.sender.send((self.out, MSG_DISCONNECT, None, None));
+        self.sender.send((self.out, MSG_DISCONNECT, JsonValue::Null));
     }
 
     fn on_message(&mut self, msg: Message) -> Result<()> {
-        if !msg.is_text(){
-            return self.out.send(Message::text("非文本消息"));
-        }
-        println!("on_message:{}", msg.as_text().unwrap());
+        println!("on_message:{:?}", msg);
 
-        let msg = msg.into_text().unwrap();
-        let json = json::parse(&msg);
-        
-        if json.is_err(){
-            return self.out.send(Message::text("JSON格式错误"));
+        if msg.is_text(){
+            if let Ok(json) = json::parse(&msg.into_text().unwrap()){
+                if json.is_array(){
+                    //将消息转换成整数数组
+                    if let Some(msg_id) = json[0].as_isize(){
+                        //玩家开始游戏，通知游戏添加精灵，然后广播
+                        //玩家键盘操作，通知游戏更新，然后广播
+                        self.sender.send((self.out, msg_id, json[1]));
+                        return  Ok(());
+                    }
+                }
+            }
         }
-
-        let json = json.unwrap();
-        if !json.is_array(){
-            return self.out.send(Message::text("数据格式错误"));
-        }
-        
-        //将消息转换成整数数组
-        if let Some(msg_id) = json[0].as_isize(){
-            //玩家开始游戏，通知游戏添加精灵，然后广播
-            //玩家键盘操作，通知游戏更新，然后广播
-            self.sender.send((self.out, msg_id, json[1].as_isize(), json[2].as_isize()));
-            Ok(())
-        }else{
-            self.out.send(Message::text("数据格式错误"))
-        }
+        return self.out.send(Message::text("JSON格式错误"));
     }
 }
 
@@ -87,8 +77,8 @@ fn main() {
         let mut game = TankGame::new();
         loop{
             //处理websocket传来的消息
-            if let Ok((sender, msg, v1, v2)) = game_receiver.try_recv(){
-                match msg{
+            if let Ok((sender, msg_id, json)) = game_receiver.try_recv(){
+                match msg_id{
                     MSG_CONNECT => {
                         //玩家连线，返回所有精灵列表
                         let sprites = game.sprites();
@@ -110,19 +100,23 @@ fn main() {
                         }
                         sender.send(Message::text(json::stringify(array)));
                     }
+
+                    MSG_START => {
+                        //玩家加入游戏
+                        
+                    }
+
                     MSG_DISCONNECT => {
                         //玩家断开链
                         //如果玩家正在游戏，删除玩家
 
                     }
-                    MSG_START => {
-                        //玩家加入游戏
-                        
-                    }
+
                     MSG_KEY_EVENT => {
                         //玩家上传按键事件
 
                     }
+
                     MSG_MOUSE_EVENT => {
                         //玩家上传鼠标事件
                     }
