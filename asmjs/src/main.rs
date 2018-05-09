9b50cf0a5abcf8f56325075808921bb8ee069679
Jia Ye <planet0104@gmail.com>
2018-05-09 10:49:53 +0800

    test

--- asmjs/src/main.rs
+++ asmjs/src/main.rs
@@ -11,6 +11,7 @@ use std::rc::Rc;
 use std::sync::{Arc, Mutex};
 
 //导入的JS帮助函数
+#[link_args = "-s ASSERTIONS=1"]
 extern "C" {
     pub fn emscripten_prompt(title: *const c_char, default_msg: *const c_char)->*mut c_char;
     pub fn emscripten_current_time_millis()->f64;
@@ -88,6 +89,7 @@ thread_local!{
     static CONTEXT: JSGameContext = JSGameContext{};
 }
 
+#[link_args = "-s ASSERTIONS=1"]
 #[no_mangle]
 pub fn request_animation_frame_callback(timestamp: f64) {
     JS.with(|js|{
@@ -97,6 +99,7 @@ pub fn request_animation_frame_callback(timestamp: f64) {
     });
 }
 
+#[link_args = "-s ASSERTIONS=1"]
 #[no_mangle]
 pub fn on_window_resize() {
     JS.with(|js|{
@@ -106,6 +109,7 @@ pub fn on_window_resize() {
     });
 }
 
+#[link_args = "-s ASSERTIONS=1"]
 #[no_mangle]
 pub fn on_resource_load(num: i32, total: i32) {
     JS.with(|js|{
@@ -115,6 +119,7 @@ pub fn on_resource_load(num: i32, total: i32) {
     });
 }
 
+#[link_args = "-s ASSERTIONS=1"]
 #[no_mangle]
 pub fn on_connect() {
     JS.with(|js|{
@@ -124,6 +129,7 @@ pub fn on_connect() {
     });
 }
 
+#[link_args = "-s ASSERTIONS=1"]
 #[no_mangle]
 pub fn on_close() {
     JS.with(|js|{
@@ -133,6 +139,7 @@ pub fn on_close() {
     });
 }
 
+#[link_args = "-s ASSERTIONS=1"]
 #[no_mangle]
 pub fn on_keyup_event(key: i32) {
     //console_log("on_keydown_up");
@@ -141,6 +148,7 @@ pub fn on_keyup_event(key: i32) {
     }
 }
 
+#[link_args = "-s ASSERTIONS=1"]
 #[no_mangle]
 pub fn on_keydown_event(key: i32) {
     //console_log("on_keydown");
@@ -149,6 +157,7 @@ pub fn on_keydown_event(key: i32) {
     }
 }
 
+#[link_args = "-s ASSERTIONS=1"]
 #[no_mangle]
 pub fn on_message(msg: *mut c_char) {
     let c_string = unsafe{ CString::from_raw(msg) };
@@ -164,6 +173,7 @@ pub fn on_message(msg: *mut c_char) {
     //console_log("on_message 222");
 }
 
+#[link_args = "-s ASSERTIONS=1"]
 #[no_mangle]
 pub unsafe fn on_binary_message(msg: *mut u8, length: usize) {
     let msg = Vec::from_raw_parts(msg, length, length);
@@ -224,6 +234,7 @@ fn console_log( msg: &str) {
     }
 }
 
+#[link_args = "-s ASSERTIONS=1"]
 #[no_mangle]
 pub fn start() {
     GAME.with(|game|{
@@ -482,6 +493,7 @@ impl GameContext for JSGameContext {
     }
 }
 
+#[link_args = "-s ASSERTIONS=1"]
 fn main(){
     println!("main.");
     // let since_the_epoch = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
