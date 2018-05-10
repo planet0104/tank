79d2b7d5a71fca8754801632769b729bd104b182
Jia Ye <planet0104@gmail.com>
2018-05-10 20:00:53 +0800

    build.sh

--- stdweb/build.sh
+++ stdweb/build.sh
@@ -1,7 +1,7 @@
 # 编译 asmjs
 cargo build --target=asmjs-unknown-emscripten --release
-copy ./target/asmjs-unknown-emscripten/release/client.js ./html/client-asm.js
+cp ./target/asmjs-unknown-emscripten/release/client.js ./html/client-asm.js
 # 编译 wasm
 cargo +nightly web build --target=wasm32-unknown-unknown --release
-copy ./target/wasm32-unknown-unknown/release/client.wasm ./html/client.wasm
-copy ./target/wasm32-unknown-unknown/release/client.js ./html/client.js
+cp ./target/wasm32-unknown-unknown/release/client.wasm ./html/client.wasm
+cp ./target/wasm32-unknown-unknown/release/client.js ./html/client.js
